04475d9574b248b83215ce6f70075e3011a2b06e
assilbekov <askhat.assilbekov@gmail.com>
2024-03-05 11:07:44 -0300

    tasks page clean imports

--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -1,24 +1,8 @@
 import type { Metadata } from "next"
-import Image from "next/image"
-import { z } from "zod"
-import { UserProfile, UserButton } from "@clerk/nextjs";
+import { UserButton } from "@clerk/nextjs";
 import { unstable_noStore as noStore } from 'next/cache';
 
-
-
-import { columns } from "./_components/columns"
-import { DataTable } from "./_components/data-table"
-import { UserNav } from "./_components/user-nav"
-import { taskSchema } from "./data/schema"
-import type { MockTask } from "./data/tasks.mock"
-import { mockTasks } from "./data/tasks.mock"
-import { CreateFinanceDialog } from "./_components/CreateFinanceDialog"
 import { Label } from "~/components/ui/label";
-import { InfoCard } from "./_components/InfoCard";
-import { CurrentValueDialog } from "./_components/CurrentValueDialog";
-import { InflationDialog } from "./_components/InflationDialog";
-import { FinancialOverview } from "./_components/FinancialOverview";
-import { YearsPlanningDialog } from "./_components/YearsPlanningDialog";
 import { api } from "~/trpc/server";
 import { ClientPage } from "./_components/ClientPage";
 
